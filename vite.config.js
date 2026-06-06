657d743c159fcfbe7648142b9bfdd6550f4d47f3
aidev0p6 <atuganick90@gmail.com>
2026-06-06 04:15:33 +0300

    Image optimizer dependancy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import { defineConfig } from 'vite'  
+import react from '@vitejs/plugin-react'  
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'  
+  
+export default defineConfig({  
+  plugins: [  
+    react(),  
+    ViteImageOptimizer({  
+      png: { quality: 80 },  
+      jpeg: { quality: 80 },  
+      jpg: { quality: 80 }  
+    })  
+  ]  
+})  
