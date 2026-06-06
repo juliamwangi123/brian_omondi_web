1dcb9d6453123eb0e903117cf0a6a9498228047d
aidev0p6 <atuganick90@gmail.com>
2026-06-06 05:20:04 +0300

    Image optimizer About Candindate

--- src/pages/Landing/sections/About.jsx
+++ src/pages/Landing/sections/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
       <div className="about__inner">
         <div className="about__image anim anim--slide-left">
           <img
-            src="/images/candidate-portrait.svg"
+            src="/images/candidate-portrait.webp"
             alt="Hon. Brian Omondi"
             loading="lazy"
           />
